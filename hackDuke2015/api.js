4c2649cb641bf5c1e7ef79e528f9d8fa1d45b046
Matt Petters <matt@petters.io>
2015-11-07 15:57:11 -0500

    Initialized meteor project and moved api to folder

---
+++ hackDuke2015/api.js
@@ -0,0 +1,12 @@
+var apiKey = '95e3454973361d26a6478ed1fd4a5e16b3d55680'
+
+var url =  "http://api.census.gov/data/timeseries/poverty/histpov2?get=PCTPOV&time=2013&RACE=1";
+
+$.ajax({                                                                                                                                                                                                        
+  type: 'GET',                                                                                                                                                                                                 
+  url: url,                                                                                                                                              
+  dataType: 'jsonp',                                                                                                                                                                                                
+  success: function(data) { console.log('Success!',data); },                                                                                                                                                                                       
+  error: function() { console.log('Uh Oh!'); },
+  jsonp: 'jsonp'                                                                                                                                                
+});
