6a0abcc003ee1e477f24704f8be7e2d09ba167bf
Matt Petters <matt@petters.io>
2015-11-07 16:37:47 -0500

    api testing

--- hackDuke2015/client/api.js
+++ hackDuke2015/client/api.js
@@ -1,10 +1,12 @@
 var apiKey = '95e3454973361d26a6478ed1fd4a5e16b3d55680';
 
-var url =  "http://api.census.gov/data/timeseries/poverty/histpov2?get=PCTPOV&time=2013&RACE=1";
+var getAllRacesPoverty2015 =  "http://api.census.gov/data/timeseries/poverty/histpov2?get=PCTPOV&time=2015&RACE=1";
+
+
 
 $.ajax({                                                                                                                                                                                                        
   type: 'GET',                                                                                                                                                                                                 
-  url: url,                                                                                                                                              
+  url: getAllRacesPoverty2015,                                                                                                                                              
   dataType: 'json',                                                                                                                                                                                                
   success: function(data) { console.log('Success!',data); },                                                                                                                                                                                       
   error: function() { console.log('Uh Oh!'); },
