4f27310d36f5486b9d948b8815957f712f1306ce
Matt Petters <matt@petters.io>
2015-11-07 17:32:55 -0500

    fuck census gov

--- hackDuke2015/client/api.js
+++ hackDuke2015/client/api.js
@@ -1,8 +1,4 @@
-var apiKey = '95e3454973361d26a6478ed1fd4a5e16b3d55680';
-
-var getAllRacesPoverty2015 =  "http://api.census.gov/data/timeseries/poverty/histpov2?get=PCTPOV&time=2015&RACE=1";
-
-
+var getAllRacesPoverty2015 = "http://api.census.gov/data/timeseries/poverty/histpov2?get=PCTPOV&time=2013&RACE=1";
 
 $.ajax({                                                                                                                                                                                                        
   type: 'GET',                                                                                                                                                                                                 
@@ -12,3 +8,4 @@ $.ajax({
   error: function() { console.log('Uh Oh!'); },
   jsonp: 'false'                                                                                                                                                
 });
+
